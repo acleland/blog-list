a0ecfc48f29fe748c69644e3dbec013df9ccb548
Andrea Cleland <andreaclelan@gmail.com>
2022-03-14 12:32:07 -0700

    created a Main component but haven't implemented anything yet

---
+++ src/views/Main/Main.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Main.css';
+
+function Main() {
+  return (
+    <section className="main">
+      <h1>This is the Main component</h1>
+    </section>
+  );
+}
+
+export default Main;
